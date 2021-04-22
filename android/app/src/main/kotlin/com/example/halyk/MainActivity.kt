577b07a10bdc20ddd03f695dc201c147e6e7e39c
Sultannis <sultan.fb29@gmail.com>
2021-04-22 19:33:02 +0600

    feat: create basic flutter project structure

---
+++ android/app/src/main/kotlin/com/example/halyk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.halyk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/halykvolunteers/halyk/MainActivity.kt
@@ -0,0 +1,6 @@
+package halykvolunteers.halyk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
